a09dcdbc39a6978cb155561528fdcf811bd79e45
tylerle2001 <tyler.le2001@outlook.com>
2025-08-12 13:35:25 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
-const nextConfig = { output: "export", images: { unoptimized: true }, basePath: isProd ? "/tylerle-portfolio" : "", assetPrefix: isProd ? "/tylerle-portfolio/" : "" };
-module.exports = nextConfig;
+module.exports = {
+  output: "export",
+  images: { unoptimized: true },
+  basePath: isProd ? "/tylerle-portfolio" : "",
+  assetPrefix: isProd ? "/tylerle-portfolio/" : "",
+};
+
