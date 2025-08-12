4becf550027847b75fa279d36e187831c9074a4c
tylerle2001 <tyler.le2001@outlook.com>
2025-08-12 13:24:14 -0500

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="mt-12 pt-4 border-t text-center text-gray-500 text-sm">
+      <p>© {new Date().getFullYear()} Tyler Le. All rights reserved.</p>
+    </footer>
+  );
+}
