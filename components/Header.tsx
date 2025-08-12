b3bf492f7a1435e706140059abb4ff6905036cc1
tylerle2001 <tyler.le2001@outlook.com>
2025-08-12 13:23:29 -0500

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="mb-10 border-b pb-4">
+      <h1 className="text-4xl font-bold">Tyler Le</h1>
+      <p className="text-lg text-gray-600">
+        Consultant • Sports Enthusiast • Aspiring Developer
+      </p>
+    </header>
+  );
+}
